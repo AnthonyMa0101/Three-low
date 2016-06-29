b59ebbb25125f46297ddc190366588841ddb447c
AnthonyMa0101 <[anthony.ma111@gmail.com]>
2016-06-29 08:46:20 -0400

    Created file

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Anthony Ma on 2016-06-29.
+//  Copyright © 2016 Anthony Ma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
